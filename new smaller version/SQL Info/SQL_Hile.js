7c14661d9b7ca9ed395dd9c9436a2fccd0b4f7e6
dziubanoah <145217862+dziubanoah@users.noreply.github.com>
2025-03-17 08:54:31 +0100

    extended switch case szenarion

--- new smaller version/SQL Info/SQL_Hile.js
+++ new smaller version/SQL Info/SQL_Hile.js
@@ -7,11 +7,11 @@ let Text_Div_2 = document.getElementById("Text_Div_2");
 let Text_Div_3 = document.getElementById("Text_Div_3");
 let Text_Div_4 = document.getElementById("Text_Div_4");
 let Text_Div_5 = document.getElementById("Text_Div_5");
-
+let InfoText = document.getElementsByClassName("InfoText")
 let Aktuelle_Seitenzahl = parseInt(Seiten_Zahl.innerHTML);
 
+
 Links_Click.addEventListener("click", function() {
-    window.alert("links");
     Aktuelle_Seitenzahl = parseInt(Seiten_Zahl.innerHTML);
     console.log(Aktuelle_Seitenzahl);
     Seiten_Zahl.innerHTML = (parseInt(Seiten_Zahl.innerHTML) - 1);
@@ -19,7 +19,6 @@ Links_Click.addEventListener("click", function() {
 });
 
 Rechts_Click.addEventListener("click", function() {
-    window.alert("rechts");
     Aktuelle_Seitenzahl = parseInt(Seiten_Zahl.innerHTML);
     console.log(Aktuelle_Seitenzahl);
     Seiten_Zahl.innerHTML = (parseInt(Seiten_Zahl.innerHTML) + 1);
@@ -31,14 +30,35 @@ function Load_Container () {
 
     Text_Div_2.style.visibility = "hidden";
     Text_Div_3.style.visibility = "hidden";
-    Text_Div_1.style.visibility = "hidden";
+    Text_Div_1.style.visibility = "visible";
+    Text_Div_4.style.visibility = "hidden";
+    Text_Div_5.style.visibility = "hidden";
 
 switch(Aktuelle_Seitenzahl) {
-    case 1: Text_Div_1.style.visibility = "visible"; break;
-    case 2: Text_Div_2.style.visibility = "visible"; break;
-    case 3: Text_Div_3.style.visibility = "visible"; break;
-    case 4: Text_Div_4.style.visibility = "visible"; break;
-    case 5: Text_Div_5.style.visibility = "visible"; break;
+    case 1: Text_Div_1.style.visibility = "visible"; 
+    start_Animation();
+    break;
+    case 2: Text_Div_2.style.visibility = "visible"; 
+    start_Animation();
+    break;
+    case 3: Text_Div_3.style.visibility = "visible"; 
+    start_Animation();
+    break;
+    case 4: Text_Div_4.style.visibility = "visible"; 
+    start_Animation();
+    break;
+    case 5: Text_Div_5.style.visibility = "visible";
+    start_Animation();
+    break;
     default: break
-    };
-};
+    }
+}
+
+function start_Animation () {
+    element.style.animation = 'none';
+    void element.offsetWidth;
+    setTimeout(function() {
+        me.style.webkitAnimation = ''; // Animation zurücksetzen
+        me.style.webkitAnimation = 'typing 5s steps(150) 1s 1 normal both'; // Animation neu starten
+    }, 10); // Kurze Verzögerung, um den Reflow abzuwarten
+}
