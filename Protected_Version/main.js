476e6a7bdc8512b49bea2318452cd18af1fdac46
dziubanoah <145217862+dziubanoah@users.noreply.github.com>
2025-04-23 14:00:51 +0200

    Passwort im Klartext anzeigen & sha256 verschlüsselung

--- Protected_Version/main.js
+++ Protected_Version/main.js
@@ -1,18 +1,22 @@
 let Passwort_Input = document.getElementById("Passwort_Input");
 let Info_Button = document.getElementById("Info_Button");
 let Anmelden = document.getElementById("Anmelden");
-
+let Klartext_Passwort = document.getElementById("Klartext_Passwort");
 
 Info_Button.addEventListener("click", function() {
     window.open("SQL_Info/SQL_Hilf.html");
 });
 
+Passwort_Input.addEventListener('input', function() {
+    Klartext_Passwort.innerHTML = Passwort_Input.value;
+})
+
 Anmelden.addEventListener("click", function() {
-    let Passwort_Value = Passwort_Input.value;
-    let Passwort_Value_verschluesselt = sha256(Passwort_Value);
-    console.log(Passwort_Value_verschluesselt)
+    let Passwort_Wert = Passwort_Input.value;
+    let Passwort_Value = sha256(Passwort_Wert);
+    console.log(Passwort_Value)
     window.alert(Passwort_Value)
-    fetch("http://localhost:8080", {
+    fetch("http://localhost:8081", {
         method: "POST",
         headers: {
             "Content-Type": "text/plain" 
@@ -24,3 +28,4 @@ Anmelden.addEventListener("click", function() {
 .then(data => console.log(data))
 .catch(error => console.log(error));
 console.log("die Payload ist " + Passwort_Value)
+
