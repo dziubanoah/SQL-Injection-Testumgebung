d1e09bc518a6f0ad347562a6902ea9b950a12031
dziubanoah <145217862+dziubanoah@users.noreply.github.com>
2025-04-23 10:31:49 +0200

    Protected version

---
+++ Protected_Version/Abfrageservernode.mjs
@@ -0,0 +1,56 @@
+import http from "http";
+import mysql from "mysql";
+import open from "open";
+let body = '';
+
+function requestlistener(request, response) {
+
+    if (request.method == "OPTIONS") {
+        console.log("POST bekommen");
+        console.log(request.method + " bekommen");
+        response.writeHead(200);
+        response.end();
+    }
+    else if (request.method == "POST") {
+        console.log(request.method + " bekommen");
+
+        request.on("data", chunk => {
+            body += chunk // Daten werden zum Body hinzugefügt.
+            console.log(body) 
+            sql_Abfrage();
+        });
+    }
+
+    request.on("end", () => {
+        console.log(body + " am req.end")
+    })
+
+    function sql_Abfrage () {
+        let ctv_DB = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "",
+            database: "ctv_hacking_db"
+        });
+
+        ctv_DB.connect(function(err) {
+            if (err) throw err;
+            console.log("Verbindung hergestellt.")
+        });
+
+        ctv_DB.query(`SELECT * FROM ctv_hacking_table WHERE User='admin' AND Passwort='${body}';`, function(error, result) {
+            console.log(result)
+            body = '';
+            if (error) throw error;
+            console.log(result)
+            console.log(result.length);
+            if (result.length > 0) {
+                open("http://localhost/phpmyadmin/");
+            }
+        })
+    }
+}
+
+const Abfrage_Server = http.createServer(requestlistener);
+
+Abfrage_Server.listen(8081);
---
+++ Protected_Version/main.js
@@ -0,0 +1,26 @@
+let Passwort_Input = document.getElementById("Passwort_Input");
+let Info_Button = document.getElementById("Info_Button");
+let Anmelden = document.getElementById("Anmelden");
+
+
+Info_Button.addEventListener("click", function() {
+    window.open("SQL_Info/SQL_Hilf.html");
+});
+
+Anmelden.addEventListener("click", function() {
+    let Passwort_Value = Passwort_Input.value;
+    let Passwort_Value_verschluesselt = sha256(Passwort_Value);
+    console.log(Passwort_Value_verschluesselt)
+    window.alert(Passwort_Value)
+    fetch("http://localhost:8080", {
+        method: "POST",
+        headers: {
+            "Content-Type": "text/plain" 
+        },
+        body: Passwort_Value
+    })
+})
+.then(response => response.text)
+.then(data => console.log(data))
+.catch(error => console.log(error));
+console.log("die Payload ist " + Passwort_Value)
